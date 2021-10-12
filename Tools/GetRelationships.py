66651b24c759bdf38435cdf11bac86327060038d
Unknown <zigzatuzoo@gmail.com>
2021-10-12 12:15:05 -0400

    Added the "new" auto auth to getrel.py

--- Tools/GetRelationships.py
+++ Tools/GetRelationships.py
@@ -2,32 +2,18 @@ import requests
 import json
 
 ''' Enter User Cookie Here '''
-userCookie = ''
+cookie = ''
+auth_url = 'https://auth.rec.net/connect/authorize?client_id=recnet&redirect_uri=https%3A%2F%2Frec.net%2Fauthenticate%2Fsilent&response_type=id_token%20token&scope=openid%20rn.api%20rn.notify%20rn.match.read%20rn.chat%20rn.accounts%20rn.auth%20rn.link%20rn.clubs%20rn.rooms&state=3b0bbf22ce1c40e7966dc6dd0f2df854&nonce=1ec7e44b909c416bbffae6b5e00ccb38&prompt=none'
+auth_header = {'Cookie': cookie}
+r = requests.get(auth_url, headers=auth_header, allow_redirects=True)
 
-authHeader= {'Host' : 'auth.rec.net',
-             'Connection' : 'close',
-             'sec-ch-ua' : '";Not A Brand";v="99", "Chromium",v="88"',
-             'sec-ch-ua-mobile' : '?0',
-             'Upgrade-Insecure-Requests' : '1',
-             'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
-             'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-             'Sec-Fetch-Site' : 'same-site',
-             'Sec-Fetch-Mode' : 'navigate',
-             'Sec-Fetch-Dest' : 'iframe',
-             'Referer' : 'https://rec.net',
-             'Accept-Encoding' : 'gzip, deflate',
-             'Accept-Language' : 'en-US,en;q=0.9',
-             'Cookie' : userCookie
-             }
-
-''' Actual Auth Request '''
-authR = requests.get('https://auth.rec.net/connect/authorize?client_id=recnet&redirect_uri=https%3A%2F%2Frec.net%2Fauthenticate%2Fsilent&response_type=id_token%20token&scope=openid%20rn.api%20rn.notify%20rn.match.read%20rn.chat%20rn.accounts%20rn.auth%20rn.link%20rn.clubs%20rn.rooms&state=db44e0c9666b477d8415b8a3366a390e&nonce=e2d5f234c23d4c05bf2b5ecdfbd4e536&prompt=none&id_token_hint=eyJhbGciOiJSUzI1NiIsImtpZCI6IjRwY2dIaFlHWXk0LTZsUDI5d0RDME1kS09ONCIsInR5cCI6IkpXVCIsIng1dCI6IjRwY2dIaFlHWXk0LTZsUDI5d0RDME1kS09ONCJ9.eyJuYmYiOjE2Mjk0ODM1OTgsImV4cCI6MTYyOTQ4Mzg5OCwiaXNzIjoiaHR0cHM6Ly9hdXRoLnJlYy5uZXQiLCJhdWQiOiJyZWNuZXQiLCJub25jZSI6IjFhZWQwMzAyODg0OTQ1ZmFhYzk5OGUzNmZjNjhjOGZmIiwiaWF0IjoxNjI5NDgzNTk4LCJhdF9oYXNoIjoidnNnbEtCME5TNkU1UUV2Y2JJWk1rdyIsInNfaGFzaCI6ImJ2M3FTV2szbXAzWW5jTEFtajd5c3ciLCJzaWQiOiJZWGdOT1M0ek1iZG9tY29VeVB5TFB3Iiwic3ViIjoiMTU2ODEzIiwiYXV0aF90aW1lIjoxNjI5NDgzNTk1LCJpZHAiOiJsb2NhbCIsImFtciI6WyJwd2QiXX0.M8mMN2oflb2rDotYOVPF75TAU8dGno2iQS0njYUX2TH23L9Kcm1E-ILbSiJNytX8EiMHfOvr_HRvHDGqyAXF3YeU2LkYXw_qCB89poizImxnJQP9-MyDVdolU_FId01QeEsRF2nuRz3lhYUt0RuUdnJfVM5Rt2wl7DuKKG_bsnLETmzSXswYJYNku8HHlqOLECBoFv7DMG3V1uFHLw1c4F7j42OP_-6mqt2y92XRwpgHOxup5X0bSCCRCYPzJiFvaDWAzFIIeMdnUhK8EEI9471V3MVCE3ju5BJoENLGtRMC9tjy88DU7TInczrqNBTVH7xdPygLpkIPDLczM4Nqkg', headers = authHeader, allow_redirects=True)
+'''Acquire token from url'''
+url = r.url
+token = url.split("access_token=")[1].split("&token_type=")[0]
     
-nonbtoken = authR.url[942:2038]
-    
-BToken = "Bearer " + nonbtoken
+BToken = "Bearer " + token
 
-print(BToken)
+print("Token: "+BToken)
 
 userHeader= {'Host':'api.rec.net',
              'Sec-Ch-Ua':'" Not A;Brand";v="99", "Chromium";v="92"',
